dba5f2a4d48aae3b172fd176e38d7e4885f884b0
kaiser <1244713586@qq.com>
2021-08-03 19:07:13 +0800

    2021.8.3

--- include/klib/version.h
+++ include/klib/version.h
@@ -5,6 +5,8 @@
 
 #pragma once
 
+#include <string>
+
 /**
  * @brief klib major verion
  */
@@ -20,8 +22,24 @@
  */
 #define KLIB_VER_PATCH 0
 
+#define STRINGIZE2(s) #s
+#define STRINGIZE(s) STRINGIZE2(s)
+
 /**
- * @brief klib version
+ * @brief klib version string
  */
-#define KLIB_VERSION \
-  (KLIB_VER_MAJOR * 10000 + KLIB_VER_MINOR * 100 + KLIB_VER_PATCH)
+#define KLIB_VERSION_STRING \
+  "v" STRINGIZE(KLIB_VER_MAJOR) "." STRINGIZE(KLIB_VER_MINOR) "." STRINGIZE(KLIB_VER_PATCH)
+
+namespace klib {
+
+/**
+ * @brief Get klib version string
+ * @return klib version string
+ */
+inline std::string klib_version() { return KLIB_VERSION_STRING; }
+
+}  // namespace klib
+
+#undef STRINGIZE2
+#undef STRINGIZE
---
+++ test/unit_test/version_test.cpp
@@ -0,0 +1,5 @@
+#include <catch2/catch.hpp>
+
+#include "klib/version.h"
+
+TEST_CASE("klib_version") { REQUIRE(klib::klib_version() == "v0.1.0"); }
