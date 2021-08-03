151765b69e98f679144ed2f83f2f9a05542ea219
kaiser <1244713586@qq.com>
2021-08-03 21:48:26 +0800

    2021.8.3

--- src/http.cpp
+++ src/http.cpp
@@ -1,16 +1,32 @@
 #include "klib/http.h"
 
 #include <cstddef>
-#include <filesystem>
+#include <memory>
 
 #include <curl/curl.h>
 
-#include "klib/detail/error.h"
+#include "klib/exception.h"
 #include "klib/util.h"
 
 // https://curl.se/libcurl/c/multi-single.html
 // https://curl.se/libcurl/c/http2-download.html
 namespace klib::http {
+
+namespace {
+
+void check_curl_correct(CURLcode code) {
+  if (code != CURLcode::CURLE_OK) {
+    throw klib::exception::RuntimeError(curl_easy_strerror(code));
+  }
+}
+
+void check_curl_correct(CURLMcode code) {
+  if (code != CURLMcode::CURLM_OK) {
+    throw klib::exception::RuntimeError(curl_multi_strerror(code));
+  }
+}
+
+}  // namespace
 
 class Request::RequestImpl {
  public:
@@ -39,27 +55,30 @@ class Request::RequestImpl {
 };
 
 Request::RequestImpl::RequestImpl() {
-  curl_global_init(CURL_GLOBAL_DEFAULT);
+  check_curl_correct(curl_global_init(CURL_GLOBAL_DEFAULT));
 
   http_handle_ = curl_easy_init();
   if (!http_handle_) {
-    detail::error("curl_easy_init() error");
+    throw klib::exception::RuntimeError("curl_easy_init() error");
   }
 
-  curl_easy_setopt(http_handle_, CURLOPT_SSL_VERIFYPEER, 1L);
-  curl_easy_setopt(http_handle_, CURLOPT_SSL_VERIFYHOST, 2L);
-  curl_easy_setopt(http_handle_, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2_0);
-  curl_easy_setopt(http_handle_, CURLOPT_CAPATH, "/etc/ssl/certs");
-  curl_easy_setopt(http_handle_, CURLOPT_CAINFO,
-                   "/etc/ssl/certs/ca-certificates.crt");
+  check_curl_correct(
+      curl_easy_setopt(http_handle_, CURLOPT_SSL_VERIFYPEER, 1L));
+  check_curl_correct(
+      curl_easy_setopt(http_handle_, CURLOPT_SSL_VERIFYHOST, 2L));
+  check_curl_correct(
+      curl_easy_setopt(http_handle_, CURLOPT_CAPATH, "/etc/ssl/certs"));
+  check_curl_correct(curl_easy_setopt(http_handle_, CURLOPT_CAINFO,
+                                      "/etc/ssl/certs/ca-certificates.crt"));
 
-  curl_easy_setopt(http_handle_, CURLOPT_FOLLOWLOCATION, 1L);
+  check_curl_correct(
+      curl_easy_setopt(http_handle_, CURLOPT_FOLLOWLOCATION, 1L));
 
   // navigator.userAgent
-  curl_easy_setopt(
+  check_curl_correct(curl_easy_setopt(
       http_handle_, CURLOPT_USERAGENT,
       "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
-      "Chrome/92.0.4515.107 Safari/537.36 Edg/92.0.902.55");
+      "Chrome/92.0.4515.107 Safari/537.36 Edg/92.0.902.55"));
 }
 
 Request::RequestImpl::~RequestImpl() {
@@ -68,58 +87,65 @@ Request::RequestImpl::~RequestImpl() {
 }
 
 void Request::RequestImpl::verbose(bool flag) {
-  curl_easy_setopt(http_handle_, CURLOPT_VERBOSE, flag);
+  check_curl_correct(curl_easy_setopt(http_handle_, CURLOPT_VERBOSE, flag));
 }
 
 void Request::RequestImpl::allow_redirects(bool flag) {
-  curl_easy_setopt(http_handle_, CURLOPT_FOLLOWLOCATION, flag);
+  check_curl_correct(
+      curl_easy_setopt(http_handle_, CURLOPT_FOLLOWLOCATION, flag));
 }
 
 void Request::RequestImpl::set_proxy(const std::string &proxy) {
-  curl_easy_setopt(http_handle_, CURLOPT_PROXY, proxy.c_str());
+  check_curl_correct(
+      curl_easy_setopt(http_handle_, CURLOPT_PROXY, proxy.c_str()));
 }
 
 void Request::RequestImpl::set_no_proxy() {
-  curl_easy_setopt(http_handle_, CURLOPT_NOPROXY, "*");
+  check_curl_correct(curl_easy_setopt(http_handle_, CURLOPT_NOPROXY, "*"));
 }
 
 void Request::RequestImpl::set_user_agent(const std::string &user_agent) {
-  curl_easy_setopt(http_handle_, CURLOPT_USERAGENT, user_agent.c_str());
+  check_curl_correct(
+      curl_easy_setopt(http_handle_, CURLOPT_USERAGENT, user_agent.c_str()));
 }
 
 Response Request::RequestImpl::get(const std::string &url) {
   Response response;
 
-  curl_easy_setopt(http_handle_, CURLOPT_URL, url.c_str());
+  check_curl_correct(curl_easy_setopt(http_handle_, CURLOPT_URL, url.c_str()));
 
-  curl_easy_setopt(http_handle_, CURLOPT_WRITEDATA, &response.text_);
-  curl_easy_setopt(http_handle_, CURLOPT_WRITEFUNCTION,
-                   RequestImpl::callback_func_std_string);
+  check_curl_correct(
+      curl_easy_setopt(http_handle_, CURLOPT_WRITEDATA, &response.text_));
+  check_curl_correct(curl_easy_setopt(http_handle_, CURLOPT_WRITEFUNCTION,
+                                      RequestImpl::callback_func_std_string));
 
-  curl_easy_setopt(http_handle_, CURLOPT_HEADERDATA, &response.header_);
-  curl_easy_setopt(http_handle_, CURLOPT_HEADERFUNCTION,
-                   callback_func_std_string);
+  check_curl_correct(
+      curl_easy_setopt(http_handle_, CURLOPT_HEADERDATA, &response.header_));
+  check_curl_correct(curl_easy_setopt(http_handle_, CURLOPT_HEADERFUNCTION,
+                                      callback_func_std_string));
 
-  auto multi_handle = curl_multi_init();
-  curl_multi_add_handle(multi_handle, http_handle_);
+  auto free_multi_handle = [this](CURLM *multi) {
+    check_curl_correct(curl_multi_remove_handle(multi, this->http_handle_));
+    check_curl_correct(curl_multi_cleanup(multi));
+  };
+  auto multi_handle = std::unique_ptr<CURLM, decltype(free_multi_handle)>(
+      curl_multi_init(), free_multi_handle);
+
+  if (!multi_handle) {
+    throw klib::exception::RuntimeError("create multi_handle error");
+  }
+
+  check_curl_correct(curl_multi_add_handle(multi_handle.get(), http_handle_));
 
   std::int32_t still_running = 1;
-
   do {
-    if (curl_multi_perform(multi_handle, &still_running) != CURLM_OK) {
-      detail::error("curl_multi_perform() error");
-    }
-
-    if (curl_multi_poll(multi_handle, nullptr, 0, 1000, nullptr) != CURLM_OK) {
-      detail::error("curl_multi_poll() error");
-    }
+    check_curl_correct(curl_multi_perform(multi_handle.get(), &still_running));
+    check_curl_correct(
+        curl_multi_poll(multi_handle.get(), nullptr, 0, 1000, nullptr));
   } while (still_running);
 
-  curl_easy_getinfo(http_handle_, CURLINFO_RESPONSE_CODE,
-                    &response.status_code_);
-
-  curl_multi_remove_handle(multi_handle, http_handle_);
-  curl_multi_cleanup(multi_handle);
+  check_curl_correct(curl_easy_getinfo(http_handle_, CURLINFO_RESPONSE_CODE,
+                                       &response.status_code_));
 
   return response;
 }
@@ -128,7 +154,7 @@ std::size_t Request::RequestImpl::callback_func_std_string(void *contents,
                                                            std::size_t size,
                                                            std::size_t nmemb,
                                                            std::string *s) {
-  s->append(static_cast<char *>(contents), size * nmemb);
+  s->append(static_cast<const char *>(contents), size * nmemb);
   return size * nmemb;
 }
 
