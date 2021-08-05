cdacd1a76af1dc9d0db286168ffee3ca7f6eeafe
kaiser <1244713586@qq.com>
2021-08-05 12:03:33 +0800

    2021.8.5

--- cmake/Check.cmake
+++ cmake/Check.cmake
@@ -1,13 +1,16 @@
-if(CMAKE_CURRENT_SOURCE_DIR STREQUAL CMAKE_CURRENT_BINARY_DIR)
+# ---------------------------------------------------------------------------------------
+# In-source build
+# ---------------------------------------------------------------------------------------
+if(${CMAKE_CURRENT_SOURCE_DIR} STREQUAL ${CMAKE_CURRENT_BINARY_DIR})
   message(FATAL_ERROR "In-source build is not allowed")
 endif()
 
 # ---------------------------------------------------------------------------------------
 # Architecture
 # ---------------------------------------------------------------------------------------
-if(CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "x86_64")
+if(${CMAKE_HOST_SYSTEM_PROCESSOR} STREQUAL "x86_64")
   message(STATUS "Architecture: x86_64")
-elseif(CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "AMD64")
+elseif(${CMAKE_HOST_SYSTEM_PROCESSOR} STREQUAL "AMD64")
   message(STATUS "Architecture: AMD64")
 else()
   message(
@@ -18,7 +21,7 @@ endif()
 # ---------------------------------------------------------------------------------------
 # System
 # ---------------------------------------------------------------------------------------
-if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+if(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
   # https://kurotych.com/development/cmake_check_linux_kernel_version/
   execute_process(
     COMMAND uname -r
@@ -33,13 +36,24 @@ endif()
 # ---------------------------------------------------------------------------------------
 # Generator
 # ---------------------------------------------------------------------------------------
-message(STATUS "CMake Generator: ${CMAKE_GENERATOR}")
+if(${CMAKE_GENERATOR} STREQUAL "Unix Makefiles")
+  execute_process(
+    COMMAND make --version
+    OUTPUT_VARIABLE GENERATOR_VERSION
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+  string(REPLACE "\n" ";" GENERATOR_VERSION ${GENERATOR_VERSION})
+  list(GET GENERATOR_VERSION 0 GENERATOR_VERSION)
+
+  message(STATUS "CMake Generator: ${CMAKE_GENERATOR} ${GENERATOR_VERSION}")
+else()
+  message(FATAL_ERROR "The system does not support: ${CMAKE_GENERATOR}")
+endif()
 
 # ---------------------------------------------------------------------------------------
 # Compiler
 # ---------------------------------------------------------------------------------------
 if(CMAKE_COMPILER_IS_GNUCXX)
-  if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 11.1.0)
+  if(${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS 11.1.0)
     message(
       FATAL_ERROR
         "GCC version must be at least 11.1.0, the current version is: ${CMAKE_CXX_COMPILER_VERSION}"
@@ -47,8 +61,8 @@ if(CMAKE_COMPILER_IS_GNUCXX)
   endif()
   message(
     STATUS "Compiler: ${CMAKE_CXX_COMPILER_ID} ${CMAKE_CXX_COMPILER_VERSION}")
-elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-  if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 12.0.0)
+elseif(${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
+  if(${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS 12.0.0)
     message(
       FATAL_ERROR
         "Clang version must be at least 12.0.0, the current version is: ${CMAKE_CXX_COMPILER_VERSION}"
@@ -65,10 +79,6 @@ endif()
 # ---------------------------------------------------------------------------------------
 if(NOT (KLIB_BUILD_STATIC OR KLIB_BUILD_SHARED))
   message(FATAL_ERROR "You need to build at least one flavor of klib")
-endif()
-
-if(CMAKE_COMPILER_IS_GNUCXX AND KLIB_BUILD_FUZZER)
-  message(FATAL_ERROR "GCC does not support libFuzzer")
 endif()
 
 if(KLIB_VALGRIND AND KLIB_SANITIZER)
