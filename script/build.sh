5ede19cc03b0d298ff21b19f033a7fa30cd1584e
kaiser <1244713586@qq.com>
2021-10-18 13:47:24 +0800

    2021.10.18

--- script/build.sh
+++ script/build.sh
@@ -39,7 +39,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   fi
   cd dependencies
 
-  kpkg install lcov doxygen catch2 fmt libarchive curl openssl tidy-html5 boost
+  kpkg install lcov doxygen catch2 libarchive tidy-html5 curl openssl fmt boost
   sudo ldconfig
 
   sudo docker run -d -p 80:80 kennethreitz/httpbin
