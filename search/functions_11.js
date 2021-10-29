5e26dfbcf90ab665e1ee0a72bf38877e330e0b87
KaiserLancelot <KaiserLancelot@users.noreply.github.com>
2021-10-29 08:57:59 +0000

    deploy: be4731d03147f4c3ea06f4d7519aee160f3346f2

--- search/functions_11.js
+++ search/functions_11.js
@@ -2,7 +2,6 @@ var searchData=
 [
   ['table_5fexists_0',['table_exists',['../classklib_1_1SqlDatabase.html#a05df40e3453e2aa26008fc9021b8d9fa',1,'klib::SqlDatabase']]],
   ['table_5fline_5fcount_1',['table_line_count',['../classklib_1_1SqlDatabase.html#ac5a1433aa1856a5b0849dfbfa8030f2a',1,'klib::SqlDatabase']]],
-  ['tcp_5fkeep_5falive_2',['tcp_keep_alive',['../classklib_1_1Request.html#a18b12946c31dc47a2733cc1379103196',1,'klib::Request']]],
-  ['text_3',['text',['../classklib_1_1Response.html#a305b99dcd743ae31007c48c639b99713',1,'klib::Response']]],
-  ['transaction_4',['transaction',['../classklib_1_1SqlDatabase.html#a49841aa41f3015eff7bf521a335fa5f8',1,'klib::SqlDatabase']]]
+  ['text_2',['text',['../classklib_1_1Response.html#a305b99dcd743ae31007c48c639b99713',1,'klib::Response']]],
+  ['transaction_3',['transaction',['../classklib_1_1SqlDatabase.html#a49841aa41f3015eff7bf521a335fa5f8',1,'klib::SqlDatabase']]]
 ];
