670955b38fc6efbdf23ddbe2dd007269d7fdf26f
kaiser <1244713586@qq.com>
2021-10-29 15:36:43 +0800

    2021.10.29

--- src/http.cpp
+++ src/http.cpp
@@ -333,7 +333,7 @@ void Request::RequestImpl::set_browser_user_agent() {
   // navigator.userAgent
   set_user_agent(
       "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
-      "Chrome/95.0.4638.40 Safari/537.36 Edg/95.0.1020.20");
+      "Chrome/95.0.4638.54 Safari/537.36 Edg/95.0.1020.38");
 }
 
 void Request::RequestImpl::set_curl_user_agent() {
