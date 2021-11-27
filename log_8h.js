86f29cfa65b366def8eed857ec780dcafa38ad2b
KaiserLancelot <KaiserLancelot@users.noreply.github.com>
2021-11-27 11:53:21 +0000

    deploy: b112bb61df293467634d5dd69e3a912f6025b6da

--- log_8h.js
+++ log_8h.js
@@ -1,6 +1,5 @@
 var log_8h =
 [
     [ "error", "log_8h.html#a2ff77ad6fbf2e2cc00499160a8c3a70d", null ],
-    [ "info", "log_8h.html#abb610194ecc3cefc8bad87940c8f1b22", null ],
-    [ "warn", "log_8h.html#a814c67e8ab648504e8d4e9c627aafdb7", null ]
+    [ "warn", "log_8h.html#a1ac4ebc31d958a77807dce7092cf8baf", null ]
 ];
