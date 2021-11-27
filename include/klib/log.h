99451a2ceb6520ca437b3a7ada08f3b58d77fc4d
kaiser <1244713586@qq.com>
2021-11-27 17:30:46 +0800

    2021.11.27

--- include/klib/log.h
+++ include/klib/log.h
@@ -12,7 +12,7 @@
 #include <fmt/core.h>
 #include <spdlog/spdlog.h>
 
-#include "detail/log.h"
+#include "detail/log_util.h"
 
 namespace klib {
 
@@ -42,7 +42,8 @@ void warn(std::string_view fmt, Args &&...args) {
  * @param args: Format string parameters
  */
 template <typename... Args>
-void error(detail::format_with_location fmt_with_loc, Args &&...args) {
+[[noreturn]] void error(detail::format_with_location fmt_with_loc,
+                        Args &&...args) {
   spdlog::default_logger_raw()->log(fmt_with_loc.loc_, spdlog::level::err,
                                     fmt::runtime(fmt_with_loc.fmt_),
                                     std::forward<Args>(args)...);
