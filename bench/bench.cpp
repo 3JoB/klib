a85aa2990b6102461bab007cfffec584fe9152f6
kaiser <1244713586@qq.com>
2021-12-22 17:22:27 +0800

    2021.12.22

--- bench/bench.cpp
+++ bench/bench.cpp
@@ -1,14 +1,124 @@
 #include <filesystem>
 #include <string>
 
-#include <boost/core/ignore_unused.hpp>
 #include <catch2/catch.hpp>
 
 #include "klib/archive.h"
 #include "klib/hash.h"
-#include "klib/markdown.h"
 #include "klib/unicode.h"
 #include "klib/util.h"
+
+TEST_CASE("compress") {
+  REQUIRE(std::filesystem::is_directory("madler-zlib-7085a61"));
+  REQUIRE(klib::folder_size("madler-zlib-7085a61") == 2984209);
+
+  BENCHMARK_ADVANCED("zip")(Catch::Benchmark::Chronometer meter) {
+    meter.measure([] { klib::exec("zip -q -r zlib.zip madler-zlib-7085a61"); });
+
+    REQUIRE(std::filesystem::is_regular_file("zlib.zip"));
+    std::filesystem::remove("zlib.zip");
+  };
+
+  BENCHMARK_ADVANCED("libarchive zip")
+  (Catch::Benchmark::Chronometer meter) {
+    meter.measure(
+        [] { klib::compress("madler-zlib-7085a61", klib::Algorithm::Zip); });
+
+    REQUIRE(std::filesystem::is_regular_file("madler-zlib-7085a61.zip"));
+    std::filesystem::remove("madler-zlib-7085a61.zip");
+  };
+
+  BENCHMARK_ADVANCED("tar gzip")(Catch::Benchmark::Chronometer meter) {
+    meter.measure(
+        [] { klib::exec("tar -zcf zlib.tar.gz madler-zlib-7085a61"); });
+
+    REQUIRE(std::filesystem::is_regular_file("zlib.tar.gz"));
+    std::filesystem::remove("zlib.tar.gz");
+  };
+
+  BENCHMARK_ADVANCED("libarchive tar gzip")
+  (Catch::Benchmark::Chronometer meter) {
+    meter.measure(
+        [] { klib::compress("madler-zlib-7085a61", klib::Algorithm::Gzip); });
+
+    REQUIRE(std::filesystem::is_regular_file("madler-zlib-7085a61.tar.gz"));
+    std::filesystem::remove("madler-zlib-7085a61.tar.gz");
+  };
+
+  BENCHMARK_ADVANCED("tar zstd")(Catch::Benchmark::Chronometer meter) {
+    meter.measure(
+        [] { klib::exec("tar --zstd -cf zlib.tar.zst madler-zlib-7085a61"); });
+
+    REQUIRE(std::filesystem::is_regular_file("zlib.tar.zst"));
+    std::filesystem::remove("zlib.tar.zst");
+  };
+
+  BENCHMARK_ADVANCED("libarchive tar zstd")
+  (Catch::Benchmark::Chronometer meter) {
+    meter.measure(
+        [] { klib::compress("madler-zlib-7085a61", klib::Algorithm::Zstd); });
+
+    REQUIRE(std::filesystem::is_regular_file("madler-zlib-7085a61.tar.zst"));
+    std::filesystem::remove("madler-zlib-7085a61.tar.zst");
+  };
+}
+
+TEST_CASE("decompress") {
+  REQUIRE(std::filesystem::exists("zlib-v1.2.11.zip"));
+  REQUIRE(std::filesystem::exists("zlib-v1.2.11.tar.gz"));
+  REQUIRE(std::filesystem::exists("zlib-v1.2.11.tar.zst"));
+
+  BENCHMARK_ADVANCED("zip")(Catch::Benchmark::Chronometer meter) {
+    meter.measure([] { klib::exec("unzip -q -o zlib-v1.2.11.zip"); });
+
+    REQUIRE(std::filesystem::is_directory("madler-zlib-7085a61"));
+    REQUIRE(klib::folder_size("madler-zlib-7085a61") == 2984209);
+    std::filesystem::remove_all("madler-zlib-7085a61");
+  };
+
+  BENCHMARK_ADVANCED("libarchive zip")
+  (Catch::Benchmark::Chronometer meter) {
+    meter.measure([] { klib::decompress("zlib-v1.2.11.zip"); });
+
+    REQUIRE(std::filesystem::is_directory("madler-zlib-7085a61"));
+    REQUIRE(klib::folder_size("madler-zlib-7085a61") == 2984209);
+    std::filesystem::remove_all("madler-zlib-7085a61");
+  };
+
+  BENCHMARK_ADVANCED("tar gzip")(Catch::Benchmark::Chronometer meter) {
+    meter.measure([] { klib::exec("tar -zxf zlib-v1.2.11.tar.gz"); });
+
+    REQUIRE(std::filesystem::is_directory("madler-zlib-7085a61"));
+    REQUIRE(klib::folder_size("madler-zlib-7085a61") == 2984209);
+    std::filesystem::remove_all("madler-zlib-7085a61");
+  };
+
+  BENCHMARK_ADVANCED("libarchive tar gzip")
+  (Catch::Benchmark::Chronometer meter) {
+    meter.measure([] { klib::decompress("zlib-v1.2.11.tar.gz"); });
+
+    REQUIRE(std::filesystem::is_directory("madler-zlib-7085a61"));
+    REQUIRE(klib::folder_size("madler-zlib-7085a61") == 2984209);
+    std::filesystem::remove_all("madler-zlib-7085a61");
+  };
+
+  BENCHMARK_ADVANCED("tar zstd")(Catch::Benchmark::Chronometer meter) {
+    meter.measure([] { klib::exec("tar --zstd -xf zlib-v1.2.11.tar.zst"); });
+
+    REQUIRE(std::filesystem::is_directory("madler-zlib-7085a61"));
+    REQUIRE(klib::folder_size("madler-zlib-7085a61") == 2984209);
+    std::filesystem::remove_all("madler-zlib-7085a61");
+  };
+
+  BENCHMARK_ADVANCED("libarchive tar zstd")
+  (Catch::Benchmark::Chronometer meter) {
+    meter.measure([] { klib::decompress("zlib-v1.2.11.tar.zst"); });
+
+    REQUIRE(std::filesystem::is_directory("madler-zlib-7085a61"));
+    REQUIRE(klib::folder_size("madler-zlib-7085a61") == 2984209);
+    std::filesystem::remove_all("madler-zlib-7085a61");
+  };
+}
 
 TEST_CASE("hash") {
   std::string str = klib::generate_random_bytes(102400);
@@ -27,64 +137,5 @@ TEST_CASE("utf") {
 
   BENCHMARK("iconv") {
     klib::exec("iconv -f=UTF-8 -t=UTF-16 -o 263060-UTF-16.txt 263060.md");
-  };
-}
-
-TEST_CASE("markdown") {
-  BENCHMARK("cmark") {
-    klib::MarkdownParser markdown(klib::read_file("263060.md", false));
-    while (markdown.has_next()) {
-      auto item = markdown.next();
-      if (item.is_heading()) {
-        boost::ignore_unused(item.as_heading());
-      } else if (item.is_paragraph()) {
-        boost::ignore_unused(item.as_paragraph());
-      }
-    }
-  };
-}
-
-TEST_CASE("compress") {
-  REQUIRE(std::filesystem::is_directory("madler-zlib-7085a61"));
-  REQUIRE(klib::folder_size("madler-zlib-7085a61") == 2984209);
-
-  BENCHMARK_ADVANCED("tar compress")(Catch::Benchmark::Chronometer meter) {
-    meter.measure(
-        [] { klib::exec("tar -zcf zlib.tar.gz madler-zlib-7085a61"); });
-
-    REQUIRE(std::filesystem::is_regular_file("zlib.tar.gz"));
-    std::filesystem::remove("zlib.tar.gz");
-  };
-
-  BENCHMARK_ADVANCED("libarchive compress")
-  (Catch::Benchmark::Chronometer meter) {
-    meter.measure(
-        [] { klib::compress("madler-zlib-7085a61", klib::Algorithm::Gzip); });
-
-    REQUIRE(std::filesystem::is_regular_file("madler-zlib-7085a61.tar.gz"));
-    std::filesystem::remove("madler-zlib-7085a61.tar.gz");
-  };
-}
-
-TEST_CASE("decompress") {
-  REQUIRE(std::filesystem::exists("zlib-v1.2.11.tar.gz"));
-  REQUIRE(klib::sha3_512_hex(klib::read_file("zlib-v1.2.11.tar.gz", true)) ==
-          "38af19362e48ec80f6565cf18245f520c8ee5348374cb0c11286f3b23cc93fd05a6a"
-          "2a2b8784f20bb2307211a2a776241797857b133056f4b33de1d363db7bb2");
-
-  BENCHMARK_ADVANCED("tar decompress")(Catch::Benchmark::Chronometer meter) {
-    meter.measure([] { klib::exec("tar -zxf zlib-v1.2.11.tar.gz"); });
-
-    REQUIRE(std::filesystem::is_directory("madler-zlib-7085a61"));
-    REQUIRE(klib::folder_size("madler-zlib-7085a61") == 2984209);
-  };
-
-  BENCHMARK_ADVANCED("libarchive decompress")
-  (Catch::Benchmark::Chronometer meter) {
-    meter.measure([] { klib::decompress("zlib-v1.2.11.tar.gz", "gzip"); });
-
-    REQUIRE(std::filesystem::is_directory("gzip/madler-zlib-7085a61"));
-    REQUIRE(klib::folder_size("gzip/madler-zlib-7085a61") == 2984209);
-    std::filesystem::remove_all("gzip");
   };
 }
