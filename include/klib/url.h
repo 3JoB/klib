1870707331fa0dc40ba37c5fad364c97ca306a90
kaiser <1244713586@qq.com>
2022-02-06 15:18:38 +0800

    2022.2.6

--- include/klib/url.h
+++ include/klib/url.h
@@ -5,4 +5,36 @@
 
 #pragma once
 
-namespace klib {}
+#include <string>
+#include <string_view>
+#include <unordered_map>
+
+namespace klib {
+
+class URL {
+ public:
+  explicit URL(std::string url);
+
+  [[nodiscard]] std::string_view schema() const { return schema_; }
+  [[nodiscard]] std::string_view host() const { return host_; }
+  [[nodiscard]] std::string_view port() const { return port_; }
+  [[nodiscard]] std::string_view path() const { return path_; }
+  [[nodiscard]] std::string_view query() const { return query_; }
+  [[nodiscard]] std::string_view fragment() const { return fragment_; }
+  [[nodiscard]] std::string_view user_info() const { return user_info_; }
+
+  [[nodiscard]] std::unordered_map<std::string, std::string> query_map() const;
+
+ private:
+  std::string url_;
+
+  std::string_view schema_;
+  std::string_view host_;
+  std::string_view port_;
+  std::string_view path_;
+  std::string_view query_;
+  std::string_view fragment_;
+  std::string_view user_info_;
+};
+
+}  // namespace klib
--- test/unit_test/url.cpp
+++ test/unit_test/url.cpp
@@ -2,4 +2,19 @@
 
 #include "klib/url.h"
 
-TEST_CASE("url", "[url]") {}
+TEST_CASE("url", "[url]") {
+  klib::URL url(
+      R"(https://www.example.com:80/path/to/myfile.html?key1=value1&key2=value2#SomewhereInTheDocument)");
+
+  REQUIRE(url.schema() == "https");
+  REQUIRE(url.host() == "www.example.com");
+  REQUIRE(url.port() == "80");
+  REQUIRE(url.path() == "/path/to/myfile.html");
+  REQUIRE(url.query() == "key1=value1&key2=value2");
+  REQUIRE(url.fragment() == "SomewhereInTheDocument");
+  REQUIRE(std::empty(url.user_info()));
+
+  const auto map = url.query_map();
+  REQUIRE(map.at("key1") == "value1");
+  REQUIRE(map.at("key2") == "value2");
+}
