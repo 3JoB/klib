1b981bfe4585217555c82750f8364b04b95676ba
kaiser <1244713586@qq.com>
2022-02-10 13:42:24 +0800

    2022.2.10

--- bench/hash.cpp
+++ bench/hash.cpp
@@ -1,30 +1,63 @@
+#include <cstddef>
+#include <cstdint>
+#include <random>
+#include <string>
+#include <unordered_set>
+
 #include <catch2/catch.hpp>
 
 #include "klib/hash.h"
 #include "klib/util.h"
 
+namespace {
+
+struct KeyHash {
+  std::size_t operator()(const std::string &key) const {
+    return klib::fast_hash(key);
+  }
+};
+
+}  // namespace
+
 TEST_CASE("fast hash", "[hash]") {
-  SECTION("64 byte") {
-    auto data = klib::generate_random_bytes(64);
-    BENCHMARK("hash") { return klib::fast_hash(data); };
-    BENCHMARK("std hash") { return std::hash<std::string>{}(data); };
+  constexpr std::int32_t times = 1000000;
+  constexpr std::size_t size = 256;
+
+  std::vector<std::string> str;
+  str.reserve(times);
+  for (std::int32_t i = 0; i < times; ++i) {
+    str.push_back(klib::generate_random_bytes(size));
   }
 
-  SECTION("256 byte") {
-    auto data = klib::generate_random_bytes(256);
-    BENCHMARK("hash") { return klib::fast_hash(data); };
-    BENCHMARK("std hash") { return std::hash<std::string>{}(data); };
-  }
+  std::random_device rd;
+  std::default_random_engine gen(rd());
+  std::uniform_int_distribution<> dis(0, times - 1);
 
-  SECTION("512 byte") {
-    auto data = klib::generate_random_bytes(512);
-    BENCHMARK("hash") { return klib::fast_hash(data); };
-    BENCHMARK("std hash") { return std::hash<std::string>{}(data); };
-  }
+  BENCHMARK_ADVANCED("std hash")
+  (Catch::Benchmark::Chronometer meter) {
+    std::unordered_set<std::string> set;
+    meter.measure([&] {
+      for (const auto &s : str) {
+        set.insert(s);
+      }
+      for (std::int32_t i = 0; i < times / 4; ++i) {
+        set.find(str[dis(gen)]);
+        set.find(klib::generate_random_bytes(size));
+      }
+    });
+  };
 
-  SECTION("1 kb") {
-    auto data = klib::generate_random_bytes(1024);
-    BENCHMARK("hash") { return klib::fast_hash(data); };
-    BENCHMARK("std hash") { return std::hash<std::string>{}(data); };
-  }
+  BENCHMARK_ADVANCED("klib fast hash")
+  (Catch::Benchmark::Chronometer meter) {
+    std::unordered_set<std::string, KeyHash> set;
+    meter.measure([&] {
+      for (const auto &s : str) {
+        set.insert(s);
+      }
+      for (std::int32_t i = 0; i < times / 4; ++i) {
+        set.find(str[dis(gen)]);
+        set.find(klib::generate_random_bytes(size));
+      }
+    });
+  };
 }
