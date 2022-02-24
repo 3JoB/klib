3f609d2f8a699064cd0d311a11b81eb732e8d20b
kaiser <1244713586@qq.com>
2022-02-24 23:05:38 +0800

    add double and string sort benchmark

--- bench/sort.cpp
+++ bench/sort.cpp
@@ -2,7 +2,9 @@
 #include <cstddef>
 #include <cstdint>
 #include <execution>
+#include <limits>
 #include <random>
+#include <string>
 #include <vector>
 
 #include <oneapi/tbb/parallel_sort.h>
@@ -10,6 +12,8 @@
 #include <boost/sort/pdqsort/pdqsort.hpp>
 #include <boost/sort/spreadsort/spreadsort.hpp>
 #include <catch2/catch.hpp>
+
+#include "klib/util.h"
 
 TEST_CASE("integer", "[sort]") {
   constexpr std::size_t size = 1000'0000;
@@ -33,7 +37,7 @@ TEST_CASE("integer", "[sort]") {
   (Catch::Benchmark::Chronometer meter) {
     auto copy = nums;
     meter.measure([&] {
-      boost::sort::spreadsort::integer_sort(std::begin(copy), std::end(copy));
+      boost::sort::spreadsort::spreadsort(std::begin(copy), std::end(copy));
     });
     REQUIRE(std::is_sorted(std::begin(copy), std::end(copy)));
   };
@@ -66,6 +70,130 @@ TEST_CASE("integer", "[sort]") {
   BENCHMARK_ADVANCED("block indirect sort")
   (Catch::Benchmark::Chronometer meter) {
     auto copy = nums;
+    meter.measure([&] {
+      boost::sort::block_indirect_sort(std::begin(copy), std::end(copy));
+    });
+    REQUIRE(std::is_sorted(std::begin(copy), std::end(copy)));
+  };
+}
+
+TEST_CASE("double", "[sort]") {
+  constexpr std::size_t size = 1000'0000;
+  std::vector<double> nums;
+  nums.reserve(size);
+
+  std::random_device rd;
+  std::default_random_engine gen(rd());
+  std::uniform_real_distribution<> dis(std::numeric_limits<double>::min(),
+                                       std::numeric_limits<double>::max());
+  for (std::size_t i = 0; i < size; ++i) {
+    nums.push_back(dis(gen));
+  }
+
+  BENCHMARK_ADVANCED("std sort")
+  (Catch::Benchmark::Chronometer meter) {
+    auto copy = nums;
+    meter.measure([&] { std::sort(std::begin(copy), std::end(copy)); });
+    REQUIRE(std::is_sorted(std::begin(copy), std::end(copy)));
+  };
+
+  BENCHMARK_ADVANCED("spread sort")
+  (Catch::Benchmark::Chronometer meter) {
+    auto copy = nums;
+    meter.measure([&] {
+      boost::sort::spreadsort::spreadsort(std::begin(copy), std::end(copy));
+    });
+    REQUIRE(std::is_sorted(std::begin(copy), std::end(copy)));
+  };
+
+  BENCHMARK_ADVANCED("pdq sort")
+  (Catch::Benchmark::Chronometer meter) {
+    auto copy = nums;
+    meter.measure(
+        [&] { boost::sort::pdqsort(std::begin(copy), std::end(copy)); });
+    REQUIRE(std::is_sorted(std::begin(copy), std::end(copy)));
+  };
+
+  BENCHMARK_ADVANCED("tbb sort")
+  (Catch::Benchmark::Chronometer meter) {
+    auto copy = nums;
+    meter.measure(
+        [&] { tbb::parallel_sort(std::begin(copy), std::end(copy)); });
+    REQUIRE(std::is_sorted(std::begin(copy), std::end(copy)));
+  };
+
+  BENCHMARK_ADVANCED("std sort parallel")
+  (Catch::Benchmark::Chronometer meter) {
+    auto copy = nums;
+    meter.measure([&] {
+      std::sort(std::execution::par, std::begin(copy), std::end(copy));
+    });
+    REQUIRE(std::is_sorted(std::begin(copy), std::end(copy)));
+  };
+
+  BENCHMARK_ADVANCED("block indirect sort")
+  (Catch::Benchmark::Chronometer meter) {
+    auto copy = nums;
+    meter.measure([&] {
+      boost::sort::block_indirect_sort(std::begin(copy), std::end(copy));
+    });
+    REQUIRE(std::is_sorted(std::begin(copy), std::end(copy)));
+  };
+}
+
+TEST_CASE("string", "[sort]") {
+  constexpr std::size_t size = 100'0000;
+  std::vector<std::string> strs;
+  strs.reserve(size);
+
+  for (std::size_t i = 0; i < size; ++i) {
+    strs.push_back(klib::generate_random_bytes(32));
+  }
+
+  BENCHMARK_ADVANCED("std sort")
+  (Catch::Benchmark::Chronometer meter) {
+    auto copy = strs;
+    meter.measure([&] { std::sort(std::begin(copy), std::end(copy)); });
+    REQUIRE(std::is_sorted(std::begin(copy), std::end(copy)));
+  };
+
+  BENCHMARK_ADVANCED("spread sort")
+  (Catch::Benchmark::Chronometer meter) {
+    auto copy = strs;
+    meter.measure([&] {
+      boost::sort::spreadsort::spreadsort(std::begin(copy), std::end(copy));
+    });
+    REQUIRE(std::is_sorted(std::begin(copy), std::end(copy)));
+  };
+
+  BENCHMARK_ADVANCED("pdq sort")
+  (Catch::Benchmark::Chronometer meter) {
+    auto copy = strs;
+    meter.measure(
+        [&] { boost::sort::pdqsort(std::begin(copy), std::end(copy)); });
+    REQUIRE(std::is_sorted(std::begin(copy), std::end(copy)));
+  };
+
+  BENCHMARK_ADVANCED("tbb sort")
+  (Catch::Benchmark::Chronometer meter) {
+    auto copy = strs;
+    meter.measure(
+        [&] { tbb::parallel_sort(std::begin(copy), std::end(copy)); });
+    REQUIRE(std::is_sorted(std::begin(copy), std::end(copy)));
+  };
+
+  BENCHMARK_ADVANCED("std sort parallel")
+  (Catch::Benchmark::Chronometer meter) {
+    auto copy = strs;
+    meter.measure([&] {
+      std::sort(std::execution::par, std::begin(copy), std::end(copy));
+    });
+    REQUIRE(std::is_sorted(std::begin(copy), std::end(copy)));
+  };
+
+  BENCHMARK_ADVANCED("block indirect sort")
+  (Catch::Benchmark::Chronometer meter) {
+    auto copy = strs;
     meter.measure([&] {
       boost::sort::block_indirect_sort(std::begin(copy), std::end(copy));
     });
