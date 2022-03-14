513d7d4b88e6871f7891599fa38b2758efb73e55
Kaiser <1244713586@qq.com>
2022-03-14 18:10:01 +0800

    bump version to 1.8.0

--- include/klib/version.h
+++ include/klib/version.h
@@ -13,12 +13,12 @@
 /**
  * @brief klib minor version
  */
-#define KLIB_VER_MINOR 7
+#define KLIB_VER_MINOR 8
 
 /**
  * @brief klib patch version
  */
-#define KLIB_VER_PATCH 1
+#define KLIB_VER_PATCH 0
 
 #define KLIB_STRINGIZE2(s) #s
 #define KLIB_STRINGIZE(s) KLIB_STRINGIZE2(s)
