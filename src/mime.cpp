f3d722f91570c2c978617b86a099b9cbb8ed4318
Kaiser <kaiserlancelot123@gmail.com>
2022-05-04 02:32:53 +0800

    More libmagic error check

--- src/mime.cpp
+++ src/mime.cpp
@@ -1,5 +1,8 @@
 #include "klib/mime.h"
 
+#include <cerrno>
+#include <cstring>
+
 #include <magic.h>
 #include <scope_guard.hpp>
 
@@ -10,15 +13,21 @@ namespace klib {
 std::string mime(const std::string &file) {
   auto magic = magic_open(MAGIC_MIME_TYPE);
   SCOPE_EXIT { magic_close(magic); };
-  if (!magic) {
-    throw RuntimeError("Unable to initialize magic library");
+  if (!magic) [[unlikely]] {
+    throw RuntimeError("Unable to initialize magic library: {}",
+                       std::strerror(errno));
   }
 
-  if (magic_load(magic, nullptr) != 0) {
+  if (magic_load(magic, nullptr) != 0) [[unlikely]] {
     throw RuntimeError("Cannot load magic database: {}", magic_error(magic));
   }
 
-  return magic_buffer(magic, std::data(file), std::size(file));
+  auto str = magic_buffer(magic, std::data(file), std::size(file));
+  if (!str) [[unlikely]] {
+    throw RuntimeError("magic_buffer() failed: {}", magic_error(magic));
+  }
+
+  return str;
 }
 
 bool is_png(const std::string &image) { return mime(image) == "image/png"; }
