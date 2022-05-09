128df276d65bab543c79e2211ccc5ff6e9b836c2
Kaiser <kaiserlancelot123@gmail.com>
2022-05-09 22:00:27 +0800

    Bump version

--- include/klib/version.h
+++ include/klib/version.h
@@ -13,12 +13,12 @@
 /**
  * @brief klib minor version
  */
-#define KLIB_VER_MINOR 20
+#define KLIB_VER_MINOR 21
 
 /**
  * @brief klib patch version
  */
-#define KLIB_VER_PATCH 1
+#define KLIB_VER_PATCH 0
 
 #define KLIB_STRINGIZE2(s) #s
 #define KLIB_STRINGIZE(s) KLIB_STRINGIZE2(s)
