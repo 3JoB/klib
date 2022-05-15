11ae02420b66f96405913d04eb0dccb311da907d
Kaiser <kaiserlancelot123@gmail.com>
2022-05-15 11:29:25 +0800

    Update user agent

--- src/http.cpp
+++ src/http.cpp
@@ -259,7 +259,7 @@ void Request::RequestImpl::set_browser_user_agent() {
   // navigator.userAgent
   set_user_agent(
       "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
-      "Chrome/101.0.4951.54 Safari/537.36 Edg/101.0.1210.39");
+      "Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.47");
 }
 
 void Request::RequestImpl::set_timeout(std::int64_t seconds) {
